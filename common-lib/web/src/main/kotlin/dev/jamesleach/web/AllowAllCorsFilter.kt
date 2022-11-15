b498333d9956700cd0e03960d32ad0dc6dfa09ab
JFL110 <typus24@hotmail.com>
2022-11-15 22:21:59 +0000

    AWS credentials & status

--- common-lib/web/src/main/kotlin/dev/jamesleach/web/AllowAllCorsFilter.kt
+++ common-lib/web/src/main/kotlin/dev/jamesleach/web/AllowAllCorsFilter.kt
@@ -13,7 +13,7 @@ class AllowAllCorsFilter : Filter {
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val response = res as HttpServletResponse
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080")
+        response.setHeader("Access-Control-Allow-Origin", "**")
         response.setHeader("Access-Control-Allow-Credentials", "true")
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH")
         response.setHeader("Access-Control-Max-Age", "3600")
