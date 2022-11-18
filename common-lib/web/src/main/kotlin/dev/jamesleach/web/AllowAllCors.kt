e7b7a529097eee8b0f43b9cd113f252a97927fea
JFL110 <typus24@hotmail.com>
2022-11-18 22:41:45 +0000

    Cors allow credentials

--- common-lib/web/src/main/kotlin/dev/jamesleach/web/AllowAllCors.kt
+++ common-lib/web/src/main/kotlin/dev/jamesleach/web/AllowAllCors.kt
@@ -7,6 +7,7 @@ import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import java.io.IOException
 import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 /**
@@ -17,19 +18,21 @@ class AllowAllCors: Filter {
 
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val response = res as HttpServletResponse
-        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Origin", (req as  HttpServletRequest).getHeader("Origin"))
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
         response.setHeader("Access-Control-Max-Age", "3600")
         response.setHeader("Access-Control-Allow-Headers", "*")
+        response.setHeader("Access-Control-Allow-Credentials", "true")
         chain.doFilter(req, res)
     }
 
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("*")
+        configuration.allowedOriginPatterns = listOf("*")
         configuration.allowedMethods = listOf("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH")
         configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
         configuration.maxAge = 3600
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
