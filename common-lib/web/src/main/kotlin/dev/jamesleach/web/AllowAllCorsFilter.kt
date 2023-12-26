7ce30bb387da11f780890d46f7d6650e4d43affd
JFL110 <typus24@hotmail.com>
2023-12-26 20:49:39 +0000

    Cors origin dynamic

--- common-lib/web/src/main/kotlin/dev/jamesleach/web/AllowAllCorsFilter.kt
+++ common-lib/web/src/main/kotlin/dev/jamesleach/web/AllowAllCorsFilter.kt
@@ -3,7 +3,9 @@ package dev.jamesleach.web
 import org.springframework.stereotype.Component
 import java.io.IOException
 import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+
 
 /**
  * Add headers to allow all CORS.
@@ -13,7 +15,8 @@ class AllowAllCorsFilter : Filter {
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val response = res as HttpServletResponse
-        response.setHeader("Access-Control-Allow-Origin", "*")
+        val request = req as HttpServletRequest
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin") ?: "*")
         response.setHeader("Access-Control-Allow-Credentials", "true")
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH")
         response.setHeader("Access-Control-Max-Age", "3600")
