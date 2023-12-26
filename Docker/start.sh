4807be0cfe93a8f16298f0131a0a6ad801fc012a
JFL110 <typus24@hotmail.com>
2023-12-26 17:09:28 +0000

    Single service experiment

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,8 +1,9 @@
-echo "Starting Services"
-java -jar digit-classification-container.jar &
-java -jar live-drawing-canvas-container.jar &
-java -jar location-container.jar &
-
-echo "Services started"
-wait < <(jobs -p)
-echo "Services stopped"
+#echo "Starting Services"
+#java -jar digit-classification-container.jar &
+#java -jar live-drawing-canvas-container.jar &
+#java -jar location-container.jar &
+#
+#echo "Services started"
+#wait < <(jobs -p)
+#echo "Services stopped"
+java -jar digit-classification-container.jar
