40ee71c0a9328f105796bc8fff2f0ed60d32e2a3
JFL110 <typus24@hotmail.com>
2024-08-26 16:47:40 +0100

    Location container

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,7 +1,4 @@
 if [[ -z "${LOCATION_MODE}" ]]; then
- echo "Starting Location Service"
- java -jar location-container.jar
-else
   echo "Starting Services"
   java -jar digit-classification-container.jar &
   java -jar live-drawing-canvas-container.jar &
@@ -9,4 +6,7 @@ else
   echo "Services started"
   wait < <(jobs -p)
   echo "Services stopped"
+else
+   echo "Starting Location Service"
+   java -jar location-container.jar
 fi
