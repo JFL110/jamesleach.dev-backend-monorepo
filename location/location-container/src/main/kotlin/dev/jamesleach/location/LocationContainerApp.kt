d2ca56dc8815ec96efc055b94607b2755db57b20
JFL110 <typus24@hotmail.com>
2024-08-26 16:09:23 +0100

    Location container

--- location/location-container/src/main/kotlin/dev/jamesleach/location/LocationContainerApp.kt
+++ location/location-container/src/main/kotlin/dev/jamesleach/location/LocationContainerApp.kt
@@ -1,11 +1,22 @@
 package dev.jamesleach.location
 
+import dev.jamesleach.location.map.MapLocationDigester
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication(scanBasePackages = ["dev.jamesleach"])
-class LocationContainerApp
+class LocationContainerApp(
+    private val mapLocationDigester: MapLocationDigester,
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        mapLocationDigester.digestLocations()
+    }
+}
+
 
 fun main(args: Array<String>) {
-    runApplication<LocationContainerApp>(*args)
+    SpringApplication.run(LocationContainerApp::class.java, *args)
+    exitProcess(0)
 }
